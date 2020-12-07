7e659078841b6b16da653f8d1f5f88b46a10e0c0
Maciej Zurad <maciej.zurad@gmail.com>
2020-12-07 22:46:35 +0100

    Adding empty day 8

---
+++ 8.py
@@ -0,0 +1,14 @@
+def part1():
+    with open('8.input', 'rt') as f:
+        for line in f:
+            pass
+
+
+def part2():
+    with open('8.input', 'rt') as f:
+        for line in f:
+            pass
+
+if __name__ == '__main__':
+    print(f'Part1: {part1()}')
+    print(f'Part2: {part1()}')
